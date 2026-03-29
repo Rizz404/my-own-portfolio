085169cb972618a29d37de0edc8d01cb28a10521
Rizqiansyah Ramadhan <rizzthenotable@gmail.com>
2026-03-30 06:16:31 +0700

    feat(view): update HomeView with personal introduction and styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div class="flex justify-between items-center bg-background/50">
+    <div>
+      <h1 class="text-3xl font-bold text-content mb-4">
+        Hello, I'm Rizqiansyah Ramadhan. Software Developer Based In Tangerang, Banten.
+      </h1>
+      <p class="text-content/80 font-medium text-lg">
+        An undergraduate Software Engineering student specializing in backend development, with
+        experience in building and managing APIs. Proficient with a diverse tech stack including Go
+        (Golang), Express, NestJS, Flask, and Laravel. Skilled in various databases and familiar
+        with mobile development using Flutter. Eager to learn and contribute to impactful projects.
+      </p>
+    </div>
+    <div>
+      <img
+        src="https://i.pinimg.com/736x/05/68/42/0568424eab5583658cf6641c69573b78.jpg"
+        alt=""
+        class="object-cover w-72 h-72 ring-2 ring-primary/20 rounded-xs"
+      />
+    </div>
+  </div>
 </template>
