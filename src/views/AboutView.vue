34e869668216e16fbf8ae274c1400da8d83d8140
Rizqiansyah Ramadhan <rizzthenotable@gmail.com>
2026-04-03 11:22:32 +0700

    feat(view): enhance AboutView with structured layout and additional content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,48 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="px-4 lg:px-8 sm:px-6 max-w-7xl">
     <h1 class="font-bold md:text-4xl text-content">About</h1>
+    <div class="flex items-center justify-between gap-12 mt-4">
+      <div class="flex items-center justify-between gap-4 shrink-0">
+        <img
+          src="https://i.pinimg.com/originals/e8/fe/59/e8fe595d3fcec5c93bb57a21dbf67081.gif"
+          alt=""
+          class="object-cover size-42 rounded-xs ring-primary ring-2"
+        />
+        <img
+          src="https://i.pinimg.com/originals/e8/fe/59/e8fe595d3fcec5c93bb57a21dbf67081.gif"
+          alt=""
+          class="object-cover size-42 rounded-xs ring-primary ring-2"
+        />
+        <img
+          src="https://i.pinimg.com/originals/e8/fe/59/e8fe595d3fcec5c93bb57a21dbf67081.gif"
+          alt=""
+          class="object-cover size-42 rounded-xs ring-primary ring-2"
+        />
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum a voluptates excepturi
+          autem porro nostrum obcaecati corporis amet odit omnis? Accusamus assumenda perspiciatis
+          quisquam quia itaque consequuntur natus praesentium nam?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus suscipit ab eos blanditiis,
+          doloremque quas. Alias, quibusdam dolorum dolores cupiditate provident doloribus, enim
+          eligendi, vel quia temporibus exercitationem unde quas.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non dicta, magnam voluptatibus
+          doloribus deserunt dolorum. Incidunt accusamus quos quidem deleniti modi quis eaque animi,
+          veniam iusto saepe illum quo ad.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ea nisi cupiditate id
+          facere illum perferendis vero optio quod explicabo ipsam possimus itaque, aliquam totam
+          sint accusamus quo. Optio, perspiciatis?
+        </p>
+      </div>
+    </div>
   </div>
 </template>
