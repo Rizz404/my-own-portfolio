89d8322d6dd8ae0131b5682e9e54fb0adc8ae3aa
Rizqiansyah <rizzthenotable@gmail.com>
2026-08-03 17:32:52 +0700

    chore: change name convention and add to folder

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import HomeView from "@/views/HomeView.vue";
+import HomeView from "@/views/user/HomeView.vue";
 
 import { createRouter, createWebHistory } from "vue-router";
 
@@ -6,12 +6,29 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", name: "Home", component: HomeView },
-    { path: "/about", name: "About", component: () => import("@/views/AboutView.vue") },
-    { path: "/blogs", name: "Blogs", component: () => import("@/views/BlogsView.vue") },
-    { path: "/blogs/:id", name: "Blog", component: () => import("@/views/BlogView.vue") },
-    { path: "/projects", name: "Projects", component: () => import("@/views/ProjectsView.vue") },
-    { path: "/projects/:id", name: "Project", component: () => import("@/views/ProjectView.vue") },
-    { path: "/uses", name: "Uses", component: () => import("@/views/UsesView.vue") },
+    { path: "/about", name: "About", component: () => import("@/views/user/AboutView.vue") },
+    { path: "/blogs", name: "Blogs", component: () => import("@/views/user/BlogView.vue") },
+    {
+      path: "/blogs/:id",
+      name: "Blog",
+      component: () => import("@/views/user/BlogDetailView.vue"),
+    },
+    {
+      path: "/projects",
+      name: "Projects",
+      component: () => import("@/views/user/ProjectView.vue"),
+    },
+    {
+      path: "/projects/:id",
+      name: "Project",
+      component: () => import("@/views/user/ProjectDetailView.vue"),
+    },
+    { path: "/uses", name: "Uses", component: () => import("@/views/user/UseView.vue") },
+    {
+      path: "/uses/:id",
+      name: "Use",
+      component: () => import("@/views/user/UseDetailView.vue"),
+    },
   ],
 });
 
