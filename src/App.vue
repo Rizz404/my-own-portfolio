f302163d3b852abdd973c35e962e727a38ff0665
Rizqiansyah <rizzthenotable@gmail.com>
2026-08-03 17:38:41 +0700

    fix(layout): enhance layout responsiveness with additional padding and margin

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@ import AppFooter from "@/components/AppFooter.vue";
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen px-4 py-4 sm:px-6 lg:px-8">
+  <div
+    class="flex items-center justify-center min-h-screen px-4 py-4 sm:px-6 md:px-8 lg:px-12 xl:px-16"
+  >
     <main class="container min-h-[94vh] flex flex-col relative max-w-7xl w-full">
       <AppHeader />
       <section class="grow">
